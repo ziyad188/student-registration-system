1624d7ee7d53bb08fdc61d77c8b4e621c82583e0
unknown <mohdziyadk@gmail.com>
2023-06-15 08:55:57 +0530

    intial upload

---
+++ src/test/java/com/net/StudentRegistration/StudentRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.StudentRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
